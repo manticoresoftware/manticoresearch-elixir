44b3c61dff99f1eb4169afa77da7de6de9197305
Adrian Nuta <adriannuta@gmail.com>
2020-09-09 17:28:31 +0200

    update mix and readme file, add ex_doc dep

--- README.md
+++ README.md
@@ -12,8 +12,7 @@ mix do deps.get, compile
 
 ## Installation
 
-If [available in Hex](https://hex.pm/docs/publish), the package can be installed
-by adding `manticoresearch` to your list of dependencies in `mix.exs`:
+Add  `manticoresearch` to your list of dependencies in `mix.exs`:
 
 ```elixir
 def deps do
@@ -22,6 +21,4 @@ end
 ```
 
 
-Documentation can be generated with [ExDoc](https://github.com/elixir-lang/ex_doc)
-and published on [HexDocs](https://hexdocs.pm). Once published, the docs can
-be found at [https://hexdocs.pm/manticoresearch](https://hexdocs.pm/manticoresearch).
+Documentation available at [HexDocs](https://hexdocs.pm/manticoresearch).
--- mix.exs
+++ mix.exs
@@ -3,34 +3,31 @@ defmodule Manticoresearch.Mixfile do
 
   def project do
     [app: :manticoresearch,
+     description: "Manticore Search client",
      version: "1.0.0",
      elixir: "~> 1.6",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
+     package: package(),
      deps: deps()]
   end
 
-  # Configuration for the OTP application
-  #
-  # Type "mix help compile.app" for more information
   def application do
-    # Specify extra applications you'll use from Erlang/Elixir
     [extra_applications: [:logger]]
   end
 
-  # Dependencies can be Hex packages:
-  #
-  #   {:my_dep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:my_dep, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.3.0"}
-  #
-  # Type "mix help deps" for more examples and options
   defp deps do
     [
       {:tesla, "~> 1.2"},
-      {:poison, "~> 3.0"}
+      {:poison, "~> 3.0"},
+      {:ex_doc, "~> 0.22", only: :dev, runtime: false}
+    ]
+  end
+  defp package() do
+    [
+      files: ~w(lib  mix.exs README* config),
+      licenses: ["Apache-2.0"],
+      links: %{"GitHub" => "https://github.com/manticoresoftware/manticoresearch-elixir"}
     ]
   end
 end
